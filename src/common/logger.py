3ff677e201ea5a07c75f62a26e7f39963ab048cc
Irving Zamora <irvingzamora28@gmail.com>
2024-02-09 06:53:19 -0600

    feat: Implement logging with console handler
    - Implement logger that logs to app.log
    - Implement a custom logger class that adds a console handler to log messages exclusively to console

---
+++ src/common/logger.py
@@ -0,0 +1,47 @@
+import logging
+import logging.config
+from logging import Logger, StreamHandler
+
+
+class CustomLogger(Logger):
+    def console(self, message, *args, **kwargs):
+        # Create a console handler
+        console_handler = StreamHandler()
+        console_handler.setFormatter(
+            logging.Formatter("[%(asctime)s] %(levelname)s: %(message)s")
+        )
+        console_handler.setLevel(logging.INFO)
+
+        # Add the console handler, log the message, and remove the handler
+        self.addHandler(console_handler)
+        self.info(message, *args, **kwargs)
+        self.removeHandler(console_handler)
+
+
+# Set the custom logger class
+logging.setLoggerClass(CustomLogger)
+
+# Configure logging
+logging.config.dictConfig(
+    {
+        "version": 1,
+        "formatters": {
+            "default": {
+                "format": "[%(asctime)s] %(levelname)s in %(module)s: %(message)s"
+            }
+        },
+        "handlers": {
+            "file": {
+                "class": "logging.FileHandler",
+                "filename": "app.log",
+                "formatter": "default",
+                "level": "DEBUG",
+            }
+        },
+        "root": {"level": "DEBUG", "handlers": ["file"]},
+    }
+)
+
+# Create a logger instance
+logger = logging.getLogger(__name__)
+logger.info("Application started")
