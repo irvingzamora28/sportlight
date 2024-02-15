f9d88723dbc695ca5483f62c6cede79bc3537d2e
Irving Zamora <irvingzamora28@gmail.com>
2024-02-15 04:48:43 -0600

    fix: Fix error of conflicting qt libraries
    - Fix error: qt.qpa.plugin: Could not load the Qt platform plugin "xcb" in "/home/lvlanson/.local/lib/python3.8/site-packages/cv2/qt/plugins" even though it was found.
    This application failed to start because no Qt platform plugin could be initialized. Reinstalling the application may fix this problem.
    
    Available platform plugins are: xcb, eglfs, linuxfb, minimal, minimalegl, offscreen, vnc, wayland-egl, wayland, wayland-xcomposite-egl, wayland-xcomposite-glx, webgl.
    
    https://forum.qt.io/topic/119109/using-pyqt5-with-opencv-python-cv2-causes-error-could-not-load-qt-platform-plugin-xcb-even-though-it-was-found/20

--- src/common/image_processor.py
+++ src/common/image_processor.py
@@ -1,9 +1,23 @@
-import cv2
 import numpy as np
 from common.utilities import get_files_in_directory
 from common.logger import logger
 import math
 import torch
+import os
+import sys
+import cv2
+
+ci_build_and_not_headless = False
+try:
+    from cv2.version import ci_build, headless
+
+    ci_and_not_headless = ci_build and not headless
+except:
+    pass
+if sys.platform.startswith("linux") and ci_and_not_headless:
+    os.environ.pop("QT_QPA_PLATFORM_PLUGIN_PATH")
+if sys.platform.startswith("linux") and ci_and_not_headless:
+    os.environ.pop("QT_QPA_FONTDIR")
 
 
 class ImageProcessor:
